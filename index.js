6a0a8ae52031c47deb0ec977f748e153d1329d37
Bruno A. da Veiga Pinheiro <bruno.pinheiro@amcom.com.br>
2017-08-29 09:33:19 -0300

    New theme

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-$(function() {
-    var e = require("typewriter"),
-        r = document.getElementById("typewriter"),
-        t = e(r).withAccuracy(95).withMinimumSpeed(10).withMaximumSpeed(20).build();
-    t.clear().type("Passionate with Clean Code, Agile, RPi, Arduino and some IOT stuffs. Bad Software its like a lie, one time she comes comes up.").waitRange(100, 800);
-});
+$(function(){var e=require("typewriter"),i=document.getElementById("typewriter"),t=e(i).withAccuracy(95).withMinimumSpeed(10).withMaximumSpeed(20).build();t.clear().type("Passionate with Clean Code, Agile, RPi, Arduino and some IOT stuffs. Bad Software its like a lie, one time she comes comes up.").waitRange(100,800)});
